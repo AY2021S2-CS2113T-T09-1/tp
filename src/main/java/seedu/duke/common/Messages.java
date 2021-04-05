1fe777b3914693a020ceaae128e3cc65e96e6218
iamakilahamed <akilahamed123@gmail.com>
2021-04-05 19:47:17 +0800

    Update help message to match the command summary

--- src/main/java/seedu/duke/common/Messages.java
+++ src/main/java/seedu/duke/common/Messages.java
@@ -26,11 +26,11 @@ public class Messages {
      * Command summary for help command.
      */
     private static final String USER_GUIDE_LINK = "https://ay2021s2-cs2113t-t09-1.github.io/tp/UserGuide.html";
-    public static final String CHECKIN_HELP = "Check-in visitor: checkin n/NAME i/LAST_4_CHARS_OF_ID [p/PHONE_NUMBER]";
+    public static final String CHECKIN_HELP = "Check-in visitor: checkin i/LAST_4_CHARS_OF_ID n/name [p/PHONE_NUMBER]";
     public static final String LIST_ALL_HELP = "List all visitors: listall";
-    public static final String LIST_CHECKED_IN_HELP = "List checked-in visitors only: list";
+    public static final String LIST_CHECKED_IN_HELP = "List checked-in visitors only: listcheckedin";
     public static final String FIND_BY_ID_HELP = "Find visitor by ID: find i/LAST_4_CHARS_OF_ID";
-    public static final String CHECKOUT_HELP = "Checkout visitor: checkout [n/NAME] i/LAST_4_CHARS_OF_ID";
+    public static final String CHECKOUT_HELP = "Checkout visitor: checkout i/LAST_4_CHARS_OF_ID";
     public static final String CLEAR_HELP = "Clear all visitor entries: clear";
     public static final String EDIT_CAPACITY_HELP = "Edit venue capacity: editmax NEW_CAPACITY";
     public static final String MOVE_STORAGE_HELP = "Move location of storage file: movestorage PATH";
